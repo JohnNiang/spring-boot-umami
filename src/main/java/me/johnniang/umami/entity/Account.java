54d7a3ba23461e37bc4acda17f6664b7b82beaf3
johnniang <johnniang@fastmail.com>
2021-03-28 11:51:11 +0800

    Create account entity

---
+++ src/main/java/me/johnniang/umami/entity/Account.java
@@ -0,0 +1,43 @@
+package me.johnniang.umami.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * Account entity.
+ *
+ * @author johnniang
+ */
+@Entity
+@Data
+public class Account {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userId;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password", length = 100)
+    private String password;
+
+    @Column(name = "is_admin")
+    @ColumnDefault("false")
+    private Boolean isAdmin;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+}
