0c3da29c5c28b9bafb50c4476b737c2503648b54
johnniang <johnniang@fastmail.com>
2021-03-28 11:40:26 +0800

    first commit

---
+++ src/main/java/me/johnniang/umami/UmamiApplication.java
@@ -0,0 +1,13 @@
+package me.johnniang.umami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmamiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmamiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/johnniang/umami/UmamiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.johnniang.umami;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmamiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
